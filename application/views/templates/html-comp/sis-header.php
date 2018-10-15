6cd89c0e1584db49dfa2ec4d3d7d85ac6887c7e7
dummybigjj <jun.magayanes05@gmail.com>
2018-10-15 21:03:44 +0800

    Update "student_registration" function in "student" controller class
    - Update sis-registration to sis multi-step registration
    - Add html components "sis-header" for sis-registration only
    - Add assets resources named "sis-assets". Use for sis-registration multi-step styling form

---
+++ application/views/templates/html-comp/sis-header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="UTF-8">
+    <meta name="description" lang="en" content="">
+    <meta name="keywords" lang="en" content="">
+    <meta name="robots" content="index, follow">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Student Information System</title>
+
+    <!-- CSS RESOURCES -->
+    <!--Import Google Icon Font-->
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <!--Import Multi Step Indicator css-->
+    <link href="<?php echo base_url('application/assets/sis_assets/css/gsi-step-indicator.min.css'); ?>" rel="stylesheet" />
+    <!--Import  Step Form Wizard css-->
+    <link href="<?php echo base_url('application/assets/sis_assets/css/tsf-step-form-wizard.min.css'); ?>" rel="stylesheet" />
+    <!--Import  demo css-->
+    <link href="<?php echo base_url('application/assets/sis_assets/css/demo.min.css'); ?>" rel="stylesheet" />
+    <!-- parseley css -->
+    <link href="<?php echo base_url('application/assets/sis_assets/plugin/parsley/css/parsley.min.css'); ?>" rel="stylesheet" />
+    <!--Font Awesome-->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- Select2-->
+    <link rel="stylesheet" href="<?php echo base_url('application/assets/vendor/select2/dist/css/select2.css'); ?>">
+    <!-- JQUERY DATETIME PICKER -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('application/assets/jquery/jquery.datetimepicker.css'); ?>">
+    
+    <!-- JQUERY RESOURCES -->
+    <script src="<?php echo base_url('application/assets/sis_assets/js/jquery-3.1.0.min.js'); ?>"></script>
+    <script src="<?php echo base_url('application/assets/sis_assets/js/demo.js'); ?>"></script>
+    <!-- Jasny Bootstrap - Input Masks-->
+    <script src="https://d19m59y37dris4.cloudfront.net/admin-premium/1-4-4/vendor/jasny-bootstrap/js/jasny-bootstrap.min.js"> </script>
+    <!-- JQUERY DATE TIME PICKER -->
+    <script src="<?php echo base_url('application/assets/jquery/jquery.datetimepicker.full.js'); ?>"></script>
+    <!-- Select2-->
+    <script src="<?php echo base_url('application/assets/vendor/select2/dist/js/select2.js'); ?>"></script>
+    <!--Plugin for validation-->
+    <script src="<?php echo base_url('application/assets/sis_assets/plugin/parsley/js/parsley.js'); ?>"></script>
+    <script src="<?php echo base_url('application/assets/sis_assets/js/tsf-wizard-plugin.js'); ?>"></script>
+
+</head>
