d928e6ca899b9b10b1280e116af157dd03d6d290
dummybigjj <jun.magayanes05@gmail.com>
2018-12-01 15:25:14 +0800

    Update SIS meta content
    Update SIS meta content

--- application/views/templates/html-comp/header.php
+++ application/views/templates/html-comp/header.php
@@ -4,8 +4,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta name="description" content="Bootstrap Admin Template">
-    <meta name="keywords" content="app, responsive, jquery, bootstrap, dashboard, admin">
+    <meta name="description" content="Student Information System">
+    <meta name="keywords" content="student information system">
     <title> Student Information System </title>
 
     <!-- Vendor styles-->
